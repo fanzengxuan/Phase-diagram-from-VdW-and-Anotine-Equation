5596aecd74c0d91163ce712e0eac3fc21e1f783a
fanzengxuan <38831807+fanzengxuan@users.noreply.github.com>
2020-05-07 19:29:27 +0200

    Update siede-tau(saturated liquid_vapour line).py
    Siede und Tau linie durch v gerechnet

--- siede-tau(saturated liquid_vapour line).py
+++ siede-tau(saturated liquid_vapour line).py
@@ -1,51 +1,54 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import optimize
+from sympy import*
 
 def exp(a):
     return (2.71828**a)
-def ln(a):
-    return (np.log(a))
 
 def Pvdw(T,v):
-    a=0.66565
+    a=0.5336
     b=3.047926e-5
     p=8.314*T/(v-b)-a/v/v
     return p
 
-def Objfun(T,v):
-    R=8.314
-    a=0.66565
-    b=3.047926e-5
-    A=23.1963
+def Objfun(v,T):
+    A=23.196
     B=3816.44
     C=-46.13
-    fun1=R*T/(v-b)-a/v/v
-    fun2=exp(A-B/(T+C))
-    return (fun1-fun2)**2
+    a=0.554
+    b=3.047926e-5  
+    return (8.314*T/(v-b)-a/v**2-exp(A-B/(T+C)))
 
-vList=list(np.linspace(1.25e-4,1e-3,500))
-#vList=vList+list(np.linspace(1.0e-4,1.3e-4,100))
-vList.reverse()
-print(vList)
-
-TList=[]
+x=Symbol('x')
+T=list(np.linspace(522.7519775125323,660,200))
+vl=[]
+vr=[]
 pList=[]
-for i in range(len(vList)):
-    if i==0:
-        sol=optimize.minimize(Objfun,500,args=(vList[0]),method='slsqp',options={'ftol':1e-25,'eps':1e-12})
-    else:
-        sol=optimize.minimize(Objfun,T,args=(vList[i]),method='slsqp',options={'ftol':1e-25,'eps':1e-12})
+for i in range(len(T)):
+    Eqn=Objfun(x,T[i])
+    sol=solve(Eqn,x)
     #print(sol)
-    T=sol.x[0]
-    p=Pvdw(T,vList[i])
-    print(T,vList[i])
-    pList.append(p)
-    TList.append(T)
-plt.plot(vList,pList)
-plt.xlabel('v')
-plt.ylabel('p')
-plt.show()
-plt.plot(TList,pList,'.')
+    x1=re(sol[0])
+    x2=re(sol[1])
+    x3=re(sol[2])
+    print(x1,x2,x3)
+    if x1<7e-5:
+        vl.append(x1)
+        vr.append(x3)
+        p=Pvdw(T[i],x1)
+        pList.append(p)
+    else:
+        vl.append(x2)
+        vr.append(x1)
+        p=Pvdw(T[i],x2)
+        pList.append(p)
+print(pList)
+
+plt.plot(vl,pList,)
+plt.plot(vr,pList,)
+plt.xlabel('v/m^3')
+plt.ylabel('p/Pa')
+plt.ylim(1.6e7,)
+plt.xlim(0,0.0004)
 plt.show()
 
